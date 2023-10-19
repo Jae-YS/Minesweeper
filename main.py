ca6297626a4c5c026641669e575192ed10ed5ccb
Jae Young Seo <jaeyoungseo@jys.local>
2023-10-19 14:18:18 -0400

    Implemented a mySQL backend to save the gametime

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import sys
 import pygame as py
-from minesweeper.constants import WIDTH, HEIGHT, SQUARE_SIZE, BACKGROUND, BLACK
+from minesweeper.constants import WIDTH, HEIGHT, SQUARE_SIZE, BACKGROUND, BLACK, DGREY
 from minesweeper.game import Game
 
 FPS = 60
 
-#setting window display
+# settinwg window display
 WIN = py.display.set_mode((WIDTH, HEIGHT))
-py.display.set_caption('Minesweeper')
+py.display.set_caption("Minesweeper")
 
 
 def get_row_col_from_mouse(pos):
@@ -24,36 +25,11 @@ def get_row_col_from_mouse(pos):
     col = x // SQUARE_SIZE
     return row, col
 
-def display_game_end(win, game_won, time_seconds):
-    """
-    Display that shows up when the game is over 
 
-    Parameters:
-        win: the window to update the display on
-        game_won: if we won or lost
-        time_seconds: how long it took to beat the game
-
-    Returns:
-        
-    """
-    
-    win.fill(BACKGROUND)
-    font = py.font.SysFont(None, 80)
-    
-    
-    if game_won: #text shown if you won the game
-        game_won_str = f"Game Won: {time_seconds} seconds"
-        text = font.render(game_won_str, True, BLACK)
-    else: #text shown if you lost the game
-        text = font.render("Game Lost", True, BLACK)
-        
-    win.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
-    py.display.update()
-    
 def reset():
     """
     Resets all variables used during the game to it's base form
-        
+
     """
     opening = True
     row, col = -1, -1
@@ -61,48 +37,111 @@ def reset():
     clicked = False
     play_again = True
     restart_game = False
-    
-    return opening, row, col, flag, clicked, play_again, restart_game
-            
 
+    return opening, row, col, flag, clicked, play_again, restart_game
+
+
+def getName(clock):
+    player_name = ""
+    font = py.font.Font(None, 36)
+
+    # Calculate the center position of the screen
+    center_x = WIDTH // 2
+    center_y = HEIGHT // 2
+
+    # Calculate the position of the input box relative to the center
+    input_box_width = 140
+    input_box_height = 32
+    input_box_x = center_x - input_box_width // 2
+    input_box_y = center_y - input_box_height // 2
+
+    input_box = py.Rect(input_box_x, input_box_y, input_box_width, input_box_height)
+
+    color_inactive = py.Color(BLACK)
+    color_active = py.Color(DGREY)
+    color = color_inactive
+    active = False
+    text = ""
+
+    while True:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                py.quit()
+                sys.exit()
+
+            if event.type == py.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = not active
+                else:
+                    active = False
+
+            if event.type == py.KEYDOWN:
+                if active:
+                    if event.key == py.K_RETURN:
+                        player_name = text
+                        text = ""
+                    elif event.key == py.K_BACKSPACE:
+                        text = text[:-1]  # Remove the last character
+                    else:
+                        text += event.unicode  # Append the character to the text
+
+        WIN.fill((255, 255, 255))  # Clear the screen
+
+        color = color_active if active else color_inactive
+
+        py.draw.rect(WIN, color, input_box, 2)
+
+        text_surface = font.render(text, True, color)
+        width = max(200, text_surface.get_width() + 10)
+        input_box.w = width
+        WIN.blit(text_surface, (input_box.x + 5, input_box.y + 5))
+
+        py.display.flip()
+        clock.tick(FPS)
+
+        if player_name:
+            break
+
+    return player_name
 
 
 def main():
-    
     run = True
     clock = py.time.Clock()
-    
-    game = Game(WIN) #create Game object as game
-    
-    opening = True #whether we are starting the game or not
-    row, col = -1, -1 #-1 as initial starting value
-    flag = False #whether or not we are adding a flag
-    clicked = False #whether or not we clicked on the screen
-    play_again = True #if we want to play again after game ends
-    restart_game = False #if we are playing again and need to reset all starting values
-    
+
+    game = Game(WIN)  # create Game object as game
+
+    opening = True  # whether we are starting the game or not
+    row, col = -1, -1  # -1 as initial starting value
+    flag = False  # whether or not we are adding a flag
+    clicked = False  # whether or not we clicked on the screen
+    play_again = True  # if we want to play again after game ends
+    restart_game = (
+        False  # if we are playing again and need to reset all starting values
+    )
+
     while play_again:
-        
         while run:
-            
-            if restart_game: #if true, restarting the game and all values used
+            if restart_game:  # if true, restarting the game and all values used
                 game.reset()
                 opening, row, col, flag, clicked, play_again, restart_game = reset()
 
             WIN.fill(BACKGROUND)
-            clock.tick(60)
-                        
+            clock.tick(FPS)
 
-            #creating event loop
+            # creating event loop
             for event in py.event.get():
-                
                 if event.type == py.QUIT:
                     run = False
-                    
+                    play_again = False
+                    break
+
                 if event.type == py.MOUSEBUTTONDOWN:
                     if event.button == 1:  # Left mouse button = click on a squares
                         pos = py.mouse.get_pos()
-                        row, col = get_row_col_from_mouse(pos) #get the row and column number 
+                        row, col = get_row_col_from_mouse(
+                            pos
+                        )  # get the row and column number
                         clicked = True
                         pass
                     elif event.button == 3:  # Right mouse button = add a flag
@@ -110,63 +149,70 @@ def main():
                         row, col = get_row_col_from_mouse(pos)
                         flag = True
                         pass
-                    
+
                 if event.type == py.KEYDOWN:
-                    if event.key == py.K_SPACE: # Spacebar = add a flag
+                    if event.key == py.K_SPACE:  # Spacebar = add a flag
                         pos = py.mouse.get_pos()
                         row, col = get_row_col_from_mouse(pos)
                         flag = True
-                        
-                    
-                opening = game.select(row, col, flag, clicked, opening) #
-                
-                flag = False
-                clicked = False 
-                
-            game.update(opening) #update the display
-            
-                        
-            continue_game, game_won = game.resume_game() #
-            
-            run = continue_game #
-                
-                
 
-        end_time = py.time.get_ticks() - game.start_time #get how long the game lasted
-        elapsed_seconds = end_time // 1000 #convert to seconds
-        
+                opening = game.select(row, col, flag, clicked, opening)  #
+
+                flag = False
+                clicked = False
+
+            game.update(opening)  # update the display
+
+            continue_game, game_won = game.resume_game()  #
+
+            run = continue_game  #
+
+        end_time = py.time.get_ticks() - game.start_time  # get how long the game lasted
+        elapsed_seconds = end_time // 1000  # convert to seconds
+
         # Delay before starting the game loop
-        py.time.delay(5000)  # 5 second delay allowing players to see their mistake
-        
-        
-        display_game_end(WIN, game_won, elapsed_seconds) #update display depending on whether or not we won or lost the game
-        
-        play_again_button, quit_button = game.display_buttons() #create the buttons used to restart or quit the game
-        
+        py.time.delay(1500)  # 1.5 second delay allowing players to see their mistake
+
+        player_name = ""
+        WIN.fill(BACKGROUND)
+
+        if game_won:
+            player_name = getName(clock)
+
+        game.display_game_end(
+            WIN, game_won, elapsed_seconds, player_name
+        )  # update display depending on whether or not we won or lost the game
+
+        (
+            play_again_button,
+            quit_button,
+        ) = (
+            game.display_buttons()
+        )  # create the buttons used to restart or quit the game
+
         quit_game = False
-        
-        #loop to determine if we are continuing the game or not, allows the button to stop flickering and wait until either button is clicked
+
+        # loop to determine if we are continuing the game or not, allows the button to stop flickering and wait until either button is clicked
         while not restart_game and not quit_game:
             for event in py.event.get():
                 if event.type == py.QUIT:
                     play_again = restart_game = False
                     quit_game = True
-            
+
                 elif event.type == py.MOUSEBUTTONDOWN:
                     mouse_pos = py.mouse.get_pos()
-                    if play_again_button.collidepoint(mouse_pos):  # Check if "Play Again" button is clicked
+                    if play_again_button.collidepoint(
+                        mouse_pos
+                    ):  # Check if "Play Again" button is clicked
                         play_again = run = restart_game = True
-                        
-                    elif quit_button.collidepoint(mouse_pos):     # Check if "Quit" button is clicked
+
+                    elif quit_button.collidepoint(
+                        mouse_pos
+                    ):  # Check if "Quit" button is clicked
                         play_again = restart_game = False
                         quit_game = True
-                        
-                        
-            
-    py.quit()    
-    
+
+    py.quit()
 
 
-
-    
 main()
--- minesweeper/game.py
+++ minesweeper/game.py
@@ -1,8 +1,24 @@
+import sys
 import pygame as py
+from saveGame import *
+from pygame.locals import *
 from minesweeper.board import Board
 from minesweeper.flag import Flag
 from minesweeper.bomb import Bomb
-from minesweeper.constants import COLS, ROWS, GREEN, WHITE, RED, BOMBS, HEIGHT, WIDTH, BLACK
+from minesweeper.constants import (
+    COLS,
+    ROWS,
+    GREEN,
+    WHITE,
+    RED,
+    BOMBS,
+    HEIGHT,
+    WIDTH,
+    BLACK,
+    WIDTH,
+    HEIGHT,
+    BACKGROUND,
+)
 
 
 class Game:
@@ -16,10 +32,41 @@ class Game:
         """
         self._init()
         py.init()
-        py.font.init()   
+        py.font.init()
         self.font = py.font.Font(None, 36)
-    
+
         self.WIN = WIN
+        self.saveGame = saveGame()
+
+    def display_game_end(self, win, game_won, time_seconds, name):
+        """
+        Display that shows up when the game is over
+
+        Parameters:
+            win: the window to update the display on
+            game_won: if we won or lost
+            time_seconds: how long it took to beat the game
+
+        Returns:
+
+        """
+
+        win.fill(BACKGROUND)
+        font = py.font.SysFont(None, 80)
+
+        if game_won:  # text shown if you won the game
+            game_won_str = f"You Won: {time_seconds} seconds"
+            text = font.render(game_won_str, True, BLACK)
+            self.saveGame.saveTime(time_seconds, name)
+        else:  # text shown if you lost the game
+            text = font.render("You Lost", True, BLACK)
+
+        win.blit(
+            text,
+            (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2),
+        )
+        py.display.update()
+        return
 
     def display_buttons(self):
         """
@@ -33,25 +80,62 @@ class Game:
         """
 
         font = py.font.SysFont(None, 40)
+        try:
+            times = self.saveGame.getTopFive()  # Try to fetch data from the database
+            if times:  # Check if the array is not empty
+                y = 100  # Starting vertical position for rendering the leaderboard
+
+                # Render the "LEADERBOARD" text and display it above the times
+                leaderboard_text = self.font.render("LEADERBOARD", True, (0, 0, 0))
+                self.WIN.blit(
+                    leaderboard_text,
+                    (WIDTH // 2 - leaderboard_text.get_width() // 2, y - 50),
+                )  # Adjust vertical position
+
+                for time in times:
+                    (
+                        name,
+                        game_time,
+                    ) = time  # Assuming that your query returns (name, time) pairs
+                    time_text = self.font.render(
+                        f"{name}: {game_time} seconds", True, (0, 0, 0)
+                    )  # Render the name and time
+                    self.WIN.blit(
+                        time_text, (WIDTH // 2 - time_text.get_width() // 2, y)
+                    )
+                    y += 50  # Adjust the vertical position for the next name and time
+            else:
+                # Handle the case where the array is empty (e.g., no data in the database)
+                no_data_text = self.font.render(
+                    "No leaderboard data available", True, (0, 0, 0)
+                )
+                self.WIN.blit(
+                    no_data_text, (WIDTH // 2 - no_data_text.get_width() // 2, y - 50)
+                )
+        except Exception as e:
+            # Handle any exceptions that may occur when fetching leaderboard data
+            print(f"Error fetching leaderboard data: {e}")
 
         # Play Again button
         play_again_button = py.Rect(WIDTH // 6, HEIGHT * 3 // 4, 200, 50)
         py.draw.rect(self.WIN, GREEN, play_again_button)
-        # format play again as string
+        # format "Play Again" as a string
         play_again_text = font.render("Play Again", True, WHITE)
-        self.WIN.blit(play_again_text, (play_again_button.x + 35, play_again_button.y + 15))
+        self.WIN.blit(
+            play_again_text, (play_again_button.x + 35, play_again_button.y + 15)
+        )
 
         # Quit button
-        quit_button = py.Rect(WIDTH * 2 // 3, HEIGHT * 3 // 4 , 120, 50)
+        quit_button = py.Rect(WIDTH * 2 // 3, HEIGHT * 3 // 4, 120, 50)
         py.draw.rect(self.WIN, RED, quit_button)
-        # format quit as string
+        # format "Quit" as a string
         quit_text = font.render("Quit", True, WHITE)
         self.WIN.blit(quit_text, (quit_button.x + 25, quit_button.y + 15))
 
-        py.display.update()
-        
+        py.display.flip()  # Update the display
+
         return play_again_button, quit_button
-        
+
     def resume_game(self):
         """
         Function that checks at the end of every move to see if we are continuing the game or not
@@ -62,31 +146,36 @@ class Game:
         Returns:
             continue_game, game_won: True if we are continuing the game or we won the game, False if we are not continuing the game or we lost the game
         """
-        
-        if self.number_of_bomb == 0: #only checks when we placed all the flag that "matches" the spot the bombs are
-            
+
+        if (
+            self.number_of_bomb == 0
+        ):  # only checks when we placed all the flag that "matches" the spot the bombs are
             self.game_won = True
             self.continue_game = False
-            
+
             for row in range(ROWS):
                 for col in range(COLS):
-                    if isinstance(self.hidden_board[row][col], Flag): #only if the current spot is a flag object, we check further
-                        if isinstance(self.board.board[row][col], Bomb): #making sure the flag object is placed in the right spot where the Bomb object should be
+                    if isinstance(
+                        self.hidden_board[row][col], Flag
+                    ):  # only if the current spot is a flag object, we check further
+                        if isinstance(
+                            self.board.board[row][col], Bomb
+                        ):  # making sure the flag object is placed in the right spot where the Bomb object should be
                             continue
-                        else: #flag was placed at an incorrect spot, but we don't stop the game and we haven't won the game yet
+                        else:  # flag was placed at an incorrect spot, but we don't stop the game and we haven't won the game yet
                             self.game_won = False
                             self.continue_game = True
                             break
-                    else: 
+                    else:
                         continue
-            
+
             return self.continue_game, self.game_won
-            
-        else: #can never reach this spot and win the game, so will always lose the game if we are not continuing the game
+
+        else:  # can never reach this spot and win the game, so will always lose the game if we are not continuing the game
             if not self.continue_game:
                 return self.continue_game, False
             return True, False
-    
+
     def update(self, opening):
         """
         Function that checks at the end of every move to see if we are continuing the game or not
@@ -94,14 +183,14 @@ class Game:
         Parameters:
             self:
             opening: True if we are starting a game, False if we have been playing the game
-            
+
         Returns:
             continue_game, game_won: True if we are continuing the game or we won the game, False if we are not continuing the game or we lost the game
         """
-        
+
         self.board.draw(self.WIN, opening, self.hidden_board)
-        
-         # Calculate elapsed time
+
+        # Calculate elapsed time
         elapsed_time = py.time.get_ticks() - self.start_time
 
         # Convert elapsed time to seconds and format as string
@@ -113,78 +202,78 @@ class Game:
         timer_rect = timer_surface.get_rect()
         timer_rect.bottomleft = (20, HEIGHT - 20)
         self.WIN.blit(timer_surface, timer_rect)
-        
+
         # format number of bomb as string
         number_of_bombs_str = f"Bombs: {self.number_of_bomb}"
-        
-        #render the number of bomb on screen
+
+        # render the number of bomb on screen
         bomb_surface = self.font.render(number_of_bombs_str, True, (0, 0, 0))
         bomb_rect = bomb_surface.get_rect()
         bomb_rect.bottomright = (WIDTH - 20, HEIGHT - 20)
         self.WIN.blit(bomb_surface, bomb_rect)
-        
+
         py.display.update()
-        
+
     def _init(self):
         """
         Private method that initializes any variables used
 
         Parameters:
             self:
-            
+
         Returns:
         """
-        
+
         # Game clock variables
         self.start_time = py.time.get_ticks()
-        
+
         self.board = Board()
-        self.hidden_board = [[0] * COLS for _ in range(ROWS)] 
-        
+        self.hidden_board = [[0] * COLS for _ in range(ROWS)]
+
         self.number_of_bomb = BOMBS
-        
+
         self.continue_game = True
         self.game_won = False
-    
+
     def reset(self):
         """
         Function that calls the private init() method to restart the game
-        
+
         Parameters:
             self:
-            
+
         Returns:
         """
         self._init()
-    
+
     def add_flag(self, row, col):
         """
         Function that adds a flag at current row, column spot
 
         Parameters:
             row, col: current spot we want to add a flag in the 2d array
-            
+
         Returns:
         """
 
-        #check to see if current spot can't add flag because what's under is already visible
+        # check to see if current spot can't add flag because what's under is already visible
         if self.board.board[row][col].visible is True:
             return
-        #if current spot already has a flag, then we are removing the flag
+        # if current spot already has a flag, then we are removing the flag
         elif isinstance(self.hidden_board[row][col], Flag):
             self.number_of_bomb += 1
             self.hidden_board[row][col] = 0
-            
-            #condition which makes sure we don't stop the game even if we go to the negative numbers of bombs and back up
+
+            # condition which makes sure we don't stop the game even if we go to the negative numbers of bombs and back up
             if self.number_of_bomb == 0:
                 self.game_won = False
                 self.continue_game = True
-                
-        #add a flag at current spot
+
+        # add a flag at current spot
         else:
-            self.number_of_bomb -=1
-            self.hidden_board[row][col] = Flag(row, col, RED)  
-         
+            self.number_of_bomb -= 1
+            self.hidden_board[row][col] = Flag(row, col, RED)
+
     def select(self, row, col, flag, clicked, opening):
         """
         Function that calls other methods to perform the desired action
@@ -194,19 +283,16 @@ class Game:
             flag: True if we are adding a flag, False if we are not
             clicked: True if we are clicking to change the visibility of current spot, False if we are not
             opening: True if we starting a game, False if we have been playing the game
-            
+
         Returns:
             Boolean: True if we are haven't started the game, False if we have
         """
-        if row < 0: #row will be -1 if we haven't started the game yet, prevent errors
+        if row < 0:  # row will be -1 if we haven't started the game yet, prevent errors
             return True
-        #adding a flag
+        # adding a flag
         if flag:
             self.add_flag(row, col)
-        #tile is clicked to change its visibility
+        # tile is clicked to change its visibility
         elif clicked:
             self.continue_game = self.board.square_clicked(row, col, opening)
         return False
-
-        
-        
